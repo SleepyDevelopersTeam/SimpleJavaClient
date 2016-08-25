2dd06320909ad5853071ba31ee299d4ee1fa0a0e
Nikita <nikita-ivanov.mail@yandex.ru>
2016-08-25 22:19:37 +0300

    Client now communicates with SDTUDTP3K

--- src/Client.java
+++ src/Client.java
@@ -1,10 +1,13 @@
 import java.net.*;
+import java.util.Random;
 import java.io.*;
 
 public class Client {
     public static void main(String[] ar) {
         int serverPort = 9090;
         String address = "127.0.0.1";
+        
+        Random behaviour = new Random();
 
         try {
             InetAddress ipAddress = InetAddress.getByName(address); // ip adress
@@ -17,22 +20,61 @@ public class Client {
             // string stram
             DataInputStream in = new DataInputStream(sin);
             DataOutputStream out = new DataOutputStream(sout);
+            
+            SUDTProtocol3K net = new SUDTProtocol3K(in, out);
 
             // keyboard stream
-            BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
-            String line = null;
-            System.out.println("Type text");
-            System.out.println();
+            // BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Client started");
+            
+            int l = behaviour.nextInt(10000);
+            if (l < 0) l = -l;
+            byte[] data = new byte[l];
+            
+            boolean open = true;
+            
+            boolean b = net.handshake(l); 
+            assert b: "Handshake failed";
+            System.out.println("Handshake send with length " + l);
 
-            while (true) {
-                line = keyboard.readLine(); // reading
-                out.writeUTF(line); //write in socket
-                out.flush();
-                line = in.readUTF(); // waiting server
-                System.out.println("The server send : " + line);
-                System.out.println();
+            while (open) {
+            	Thread.sleep(50);
+                float rnd = behaviour.nextFloat();
+                if (rnd < 0.005F)
+                {
+                	l = behaviour.nextInt(10000);
+                	data = new byte[l];
+                	b = net.changeDataLength(l);
+                	assert b : "New length set failed";
+                	System.out.println("New length: " + l);
+                	continue;
+                }
+                else if (rnd < 0.015F)
+                {
+                	b = net.sendFoneResetCommand();
+                	assert b : "Fone reset failed";
+                	System.out.println("Fone reset");
+                	continue;
+                }
+                else if (rnd < 0.02F)
+                {
+                	b = net.closeConnection();
+                	assert b : "Invalid connection close";
+                	System.out.println("Connection closed");
+                	open = false;
+                	break;
+                }
+                else
+                {
+                	behaviour.nextBytes(data);
+                	assert net.sendData(data);
+                	System.out.print(".");
+                	continue;
+                }
             }
         } catch (Exception x) {
+        	System.out.println("Error occured, stopping");
+        	System.out.println(x.getMessage());
             x.printStackTrace();
         }
     }
